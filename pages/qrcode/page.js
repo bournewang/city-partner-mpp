fcac438c483666b522f7bd7c414d90e9a1f60c1d
Bourne <xiaopei0206@icloud.com>
2024-03-23 11:40:36 +0800

    fix links in userpage;

--- pages/qrcode/page.js
+++ pages/qrcode/page.js
@@ -2,6 +2,11 @@ Page({
   data: {
   },
   onLoad() {
-    
+    let {user} = getApp().store.getState()
+    if (user.challenge_id) {
+      wx.setNavigationBarTitle({
+        title: '我的保荐码'
+      })
+    }
   }
 })
--- pages/recommends/page.js
+++ pages/recommends/page.js
@@ -6,5 +6,13 @@ Page({
   },
   onLoad() {
     userApi.recommends()
+
+    let {user} = getApp().store.getState()
+    if (user.challenge_id) {
+      wx.setNavigationBarTitle({
+        title: '我的保荐'
+      })
+    }
+
   }
 })
